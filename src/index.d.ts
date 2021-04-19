103c2b8ef1354e2bcd2d60c17d52d9737d5173c2
Timo Bechtel <hi@timobechtel.com>
2021-04-19 15:59:54 +0200

    refactor: switch to typescript

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@compactjs/cap';
-/**
- * Limits value to range from min to max
- */
-export function cap(value: number, min: number, max?: number): number;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-export const cap = (value, min, max) => {
+/**
+ * Limits value to range from min to max
+ */
+export function cap(value: number, min: number, max?: number): number {
 	if (max === undefined) return Math.min(value, min);
 	if (max < min) [min, max] = [max, min];
 	return Math.min(Math.max(value, min), max);
-};
+}
